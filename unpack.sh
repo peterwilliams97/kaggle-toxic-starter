1ed04e2cd438c36bbeb7af28731f75a43f200f61
Peter Williams <peter.williams@papercut.com>
2018-03-16 17:42:39 +1100

    typos

--- unpack.sh
+++ unpack.sh
@@ -5,12 +5,12 @@ pushd ~/data/
 mkdir models
 cd models
 mkdir word2vec
-cp GoogleNews-vectors-negative300.bin.gz word2vec
+cp ../GoogleNews-vectors-negative300.bin.gz word2vec
 cd word2vec
 gzip -d GoogleNews-vectors-negative300.bin.gz
 cd ..
 mkdir fasttext
-cp crawl-300d-2M.vec.zip fasttext
+cp ../crawl-300d-2M.vec.zip fasttext
 cd fasttext
 unzip crawl-300d-2M.vec.zip
 
